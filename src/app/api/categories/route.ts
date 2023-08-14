93ce16667041b1587eef3b2647a1e1cc5b38af0d
Muhammad Zohaib Ul Hassan <zohaibfas@gmail.com>
2023-08-14 16:10:31 +0500

    prisma setup and model creations

---
+++ src/app/api/categories/route.ts
@@ -0,0 +1,18 @@
+import { PrismaClient } from "@prisma/client";
+import { NextResponse } from "next/server";
+const prisma = new PrismaClient();
+
+//FETCH ALL THE CATEGORIES
+
+export const GET = async () => {
+  try {
+    const categories = await prisma.category.findMany();
+    return new NextResponse(JSON.stringify(categories), { status: 200 });
+  } catch (error) {
+    console.log(error);
+    return new NextResponse(
+      JSON.stringify({ message: "something went wrong" }),
+      { status: 500 }
+    );
+  }
+};
